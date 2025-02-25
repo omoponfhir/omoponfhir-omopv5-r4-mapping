2c5bb44801810c4a9338a5bb52f2c3ebc8bf760b
Myung Choi <myungchoi@gmail.com>
2025-02-24 21:56:16 -0500

    OMOP concept voc to FHIR system to keep the name

--- src/main/java/edu/gatech/chai/omoponfhir/local/dao/FhirOmopVocabularyMapImpl.java
+++ src/main/java/edu/gatech/chai/omoponfhir/local/dao/FhirOmopVocabularyMapImpl.java
@@ -126,7 +126,7 @@ public class FhirOmopVocabularyMapImpl extends BaseFhirOmopMap implements FhirOm
 
 	@Override
 	public String getFhirSystemNameFromOmopVocabulary(String omopVocabulary) {
-		String retv = "None";
+		String retv = omopVocabulary;
 		String sql = "SELECT * FROM FhirOmopVocabularyMap where omop_vocabulary_id=?";
 
 		try (Connection conn = this.connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
